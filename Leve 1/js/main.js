b6df7d87398de277852dac83ef90b9f3bb36384a
ch-moez <43502810+ch-moez@users.noreply.github.com>
2019-12-01 12:15:41 +0100

    JQUERY CHECKPOINT
    JQUERY CHECKPOINT
    01/12/2019

--- Leve 1/js/main.js
+++ Leve 1/js/main.js
@@ -99,3 +99,65 @@ function chargeFontFamily(){
  //myTextArea.text = fontFamilybySelectTag;
 
 }// end function chargeFontFamilly
+
+
+//_______________________JQuery__________________________________
+//$(document).ready(function(){
+    //alert("jquery");
+   // $("#c1").click(function(){
+        //alert("jquery");
+        //$("#course1").animate({height: "600px"});
+   // });
+//});
+//_______________________END JQuery______________________________
+
+
+
+// ______________script modal Popup_________________________
+//Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+//var btnC1 = document.getElementById("c1");
+//var btnC2 = document.getElementById("c2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+/*btnC1.onclick = function() {
+  modal.style.display = "block";
+  console.info("btn.onclick");
+}
+
+btnC2.onclick = function() {
+    modal.style.display = "block";
+    console.info("btn.onclick");
+  }
+  */
+  /*
+btnC3.onclick = function() {
+    modal.style.display = "block";
+    console.info("btn.onclick");
+  }
+*/
+//_________show the Model PopUp__________________________
+function displayModelPopUp(){
+    //alert("clicked");
+  modal.style.display = "block";
+  console.info("function displayModelPopUp()");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// _____________script END modal Popup_________________________
---
+++ Leve 1/js/mainJquery.js
@@ -0,0 +1,10 @@
+
+//_______________________JQuery__________________________________
+$(document).ready(function(){
+    //alert("jquery");
+    $("#c1").click(function(){
+        //alert("jquery");
+        //$("#course1").animate({height: "600px"});
+    });
+});
+//_______________________END JQuery______________________________
